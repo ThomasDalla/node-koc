8a7598db0d0db17f79d006c5d30b9b1602b1c414
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-07-19 00:04:34 +0000

    updated test

--- test/test.js
+++ test/test.js
@@ -1,48 +1,10 @@
 var should = require('chai').should(),
     koc = require('../koc'),
-    escape = koc.escape,
-    unescape = koc.unescape;
+    test = koc.test;
 
-describe('#escape', function() {
-  it('converts & into &amp;', function() {
-    escape('&').should.equal('&amp;');
-  });
-
-  it('converts " into &quot;', function() {
-    escape('"').should.equal('&quot;');
-  });
-
-  it('converts \' into &#39;', function() {
-    escape('\'').should.equal('&#39;');
-  });
-
-  it('converts < into &lt;', function() {
-    escape('<').should.equal('&lt;');
-  });
-
-  it('converts > into &gt;', function() {
-    escape('>').should.equal('&gt;');
-  });
-});
-
-describe('#unescape', function() {
-  it('converts &amp; into &', function() {
-    unescape('&amp;').should.equal('&');
-  });
-
-  it('converts &quot; into "', function() {
-    unescape('&quot;').should.equal('"');
-  });
-
-  it('converts &#39; into \'', function() {
-    unescape('&#39;').should.equal('\'');
-  });
-
-  it('converts &lt; into <', function() {
-    unescape('&lt;').should.equal('<');
-  });
-
-  it('converts &gt; into >', function() {
-    unescape('&gt;').should.equal('>');
+describe('#test', function() {
+  var result = test();
+  it('should respond "hellow, world"', function() {
+    result.should.equal("hellow, world");
   });
 });
