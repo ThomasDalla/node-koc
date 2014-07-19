dc26e2cf7af7c6ac3d62176be965f5a5664f38f4
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-07-19 01:23:02 +0000

    added parseBattlefield

--- test/test.js
+++ test/test.js
@@ -1,10 +1,29 @@
 var should = require('chai').should(),
-    koc = require('../koc'),
-    test = koc.test;
+    fs     = require('fs'),
+    koc    = require('../koc');
 
-describe('#test', function() {
-  var result = test();
-  it('should respond "hellow, world"', function() {
-    result.should.equal("hellow, world");
-  });
-});
+describe('#battlefield' , function() {
+  var htmlPaths = [
+    'test/html/battlefield_xhr_logged-out.html',
+    'test/html/battlefield_full_logged-out.html'
+  ];
+  for(var iHtml in htmlPaths) {
+    var htmlPath = htmlPaths[iHtml];
+    console.log('Parsing battlefield from ' + htmlPath);
+    var html     = fs.readFileSync(htmlPath, 'utf8');
+    var result   = koc.parseBattlefield(html);
+    it('should have 20 records', function() {
+      result.length.should.equal(20);
+    });
+    for(var index in result) {
+      var player = result[index];
+      it('each player should have all properties', function() {
+        var requiredFields = ['userid','alliance','username','armySize','race','gold','rank'];
+        for(var i in requiredFields){
+          player.should.have.property(requiredFields[i]);
+          player[requiredFields[i]].should.not.be.undefined;
+        }
+      });
+    }
+  }
+} );
