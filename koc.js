480f8f639449ae2314939612d9afa0c4a31b6cb2
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-07-21 07:39:23 +0000

    login, register, verify

--- koc.js
+++ koc.js
@@ -1,28 +1,334 @@
+var Q       = require('q');
+var request = Q.denodeify(require('request'));
+var util   = require("util");
+const KOC_SESSION_COOKIE = "koc_session";
+const KOC_HOST           = "http://www.kingsofchaos.com";
+const KOC_RECAPTCHA_URL  = "http://www.google.com/recaptcha/api/challenge?k=6LcvaQQAAAAAACnjh5psIedbdyYzGDb0COW82ruo";
+const RECAPTCHA_IMAGE   = "%simage?c=%s"; // path to the ReCaptcha image from (server, challenge)
+var koc = {
+    KOC_SESSION_COOKIE: ""
+};
 
-var koc = {};
+var validateEmail = function(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
 
+var getRecaptchaImage = function(server, challenge) {
+    return util.format(RECAPTCHA_IMAGE, server, challenge);
+};
+
+var getTuring = function(html) {
+    var re = /name="turing"\s*value="([^"]+)">/gmi; 
+    var m  = re.exec(html);
+    if(m!==null)
+        return m[1];
+    return html;
+}
+
+koc.getRequestOptions = function(method, page, post_data, xhr) {
+    var koc_session = "";
+    if(this.KOC_SESSION_COOKIE !== undefined && this.KOC_SESSION_COOKIE.length>0) {
+        koc_session = " " + KOC_SESSION_COOKIE + "=" + this.KOC_SESSION_COOKIE;
+    }
+    var options = {
+        url:   KOC_HOST+'/'+page,
+        method: method,
+        headers: {
+            'Cookie': "country=XO;" + koc_session,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+            'Referer' : KOC_HOST + '/'
+        },
+        followAllRedirects: true
+    };
+    if( xhr===true ) {
+        console.log("sending an XHR request");
+        options.headers["X-Requested-With"] = "XMLHttpRequest";
+        options.headers["Accept"] = "application/json, text/javascript, */*";
+        options.headers["Origin"] = "http://www.kingsofchaos.com";
+    }
+    if(post_data!==undefined && post_data!==null)
+        options.form = post_data;
+    return options;
+};
+
+var getErrorMessage = function( html ) {
+    var re = /<h3>Error<\/h3>([^<]+)</gmi;
+    var m  = re.exec( html );
+    if( m !== null) {
+        return m[1].trim();
+    }
+    return "Unknown Error";
+};
+
+koc.updateKocSession = function( headers ) {
+    var setCookie = headers['set-cookie'];
+    var _koc = this;
+    if( setCookie !== undefined && setCookie.length>0 ) {
+        setCookie.forEach( function( cookieString ) {
+            var cookie       = cookieString.split(';')[0];
+            var cookieSplit  = cookie.split('=');
+            var cookieName   = cookieSplit[0];
+            var cookieValue  = cookieSplit[1] || '';
+            if( cookieName == KOC_SESSION_COOKIE ) {
+                console.log("got a new koc_session: " + cookieValue);
+                _koc.KOC_SESSION_COOKIE = cookieValue;
+            }
+        });
+    }
+};
+
+koc.createRequestPromise = function( options, onSuccess ) {
+    var _koc = this;
+    var p = request(options)
+    .then( function(res) {
+        var response = res[0];
+        var body     = res[1];
+        _koc.updateKocSession(response.headers);
+        if (response.statusCode != 200) {
+            return {
+                success: false,
+                error: "Wrong response from the server",
+                details: "Got status " + response.statusCode,
+                KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE
+            };
+        }
+        if(response.request.path == "/error.php") {
+            return {
+                success: false,
+                error: getErrorMessage( body ),
+                KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE
+            };
+        }
+        if(response.request.path == "/verify.php") {
+            return {
+                success: false,
+                error: "You must verify your account (e-mail) before you can play Kings of Chaos",
+                KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE
+            };
+        }
+        return onSuccess(response, body);
+    })
+    .fail( function(error) {
+        return {
+            success: false,
+            error: "A connection error occurred",
+            details: error,
+            KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE
+        };
+    });
+    return p;
+};
+
+/**
+ * Parses a battlefield page in HTML format and returns a clean JS Object version
+ * @param {Text} html
+ * @return {Object} players
+ */
 koc.parseBattlefield = function(html) {
-    var re = /<tr class="player" user_id="([0-9]+)"\s*>\s*<td[^>]*><a href="alliances\.php\?id=([^"]*)">[^<]*<\/a><\/td>\s*<td><a[^>]*>([^<]+)<\/a><\/td>[^>]*>([0-9,]+)<\/td>[^>]*>\s*([A-Za-z0-9-_]+)\s*<\/td>[^>]*>([^G]+)Gold<\/td>[^>]*>([^<]+)<\/td>/gmi;
+    var re = /<a href="alliances\.php\?id=([^"]*)">[^<]*<\/a><\/td>\s*<td><a class="player" href="\/stats\.php\?id=([0-9]+)"\s*>([^<]+)<\/a><\/td>[^>]*>([0-9,]+)<\/td>[^>]*>\s*([A-Za-z0-9-_]+)\s*<\/td>[^>]*>([^G]+)Gold<\/td>[^>]*>([^<]+)<\/td>/gmi;
     var players = [];
     var m;
     while ((m = re.exec(html)) !== null) {
         if (m.index === re.lastIndex) {
             re.lastIndex++;
         }
-        // View your result using the m-variable.
-        // eg m[0] etc.
         var player = {
-          userid   : m[1],
-          alliance : m[2],
-          username : m[3],
-          armySize : m[4],
-          race     : m[5],
-          gold     : m[6],
-          rank     : m[7]
+            alliance : m[1],
+            userid   : m[2],
+            username : m[3],
+            armySize : m[4],
+            race     : m[5],
+            gold     : m[6],
+            rank     : m[7]
         };
         players.push(player);
-    };
+    }
     return players;
 };
+
+koc.requestPage = function(method, page, post_data, onSuccess, xhr) {
+    var _koc = this;
+    console.log("inside requestPage");
+    // If no koc_session, need to request one
+    if( _koc.KOC_SESSION_COOKIE === undefined || !_koc.KOC_SESSION_COOKIE.length ) {
+        // Request only headers to get a new koc_session
+        console.log("need a koc_session");
+        var p = request(_koc.getRequestOptions("HEAD",""))
+        .then( function( result ) {
+            var response = result[0];
+            _koc.updateKocSession( response.headers );
+            return _koc.createRequestPromise(_koc.getRequestOptions(method, page, post_data, xhr), onSuccess);
+        } )
+        .fail( function( error ) {
+            return {
+                success: false,
+                error: "could not get a koc_session",
+                details: error
+            };
+        } );
+        return p;
+    }
+    return _koc.createRequestPromise(_koc.getRequestOptions(method, page, post_data, xhr), onSuccess);
+};
+
+koc.login = function(username, password) {
+    var _koc = this;
+    console.log("starting login");
+    var p = _koc.requestPage("POST", "login.php", {
+        'usrname' : username,
+        'peeword': password
+    }, function(response, body) {
+        //TODO: Parse the body (camp)
+        return {
+            success: true,
+            KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE,
+            todo: body.substr(0,100)
+        };
+    });
+    return p;
+};
+
+koc.getLoginCaptcha = function() {
+    // load recaptcha dynamic script
+    var p = request({url:KOC_RECAPTCHA_URL})
+        .then(function(result){
+            var response = result[0];
+            if (response.statusCode != 200) {
+                return {
+                    success: false,
+                    error: "Wrong response from the server",
+                    details: "Got status " + response.statusCode
+                };
+            }
+            var body     = result[1];
+            // get the challenge & server
+            var reChallenge = /challenge\s*:\s*'([^']+)',/gmi;
+            var reServer    = /server\s*:\s*'([^']+)',/gmi;
+            var mChallenge  = reChallenge.exec(body);
+            var mServer     = reServer.exec(body);
+            if( mChallenge === null || mServer === null ) {
+                return {
+                    success: false,
+                    error: "Failed to read challenge or server",
+                    details: {
+                        challenge: mChallenge,
+                        server: mServer
+                    }
+                };
+            }
+            var server    = mServer[1];
+            var challenge = mChallenge[1];
+            return {
+                success: true,
+                image: getRecaptchaImage(server, challenge),
+                server: server,
+                challenge: challenge
+            };
+        })
+        .fail(function(error){
+            return {
+                success: false,
+                error: "Error getting the captcha",
+                details: error
+            };
+        });
+    return p;
+};
+
+koc.register = function(race, username, password, email, challenge, challenge_response) {
+    if(!validateEmail(email)) {
+        return Q({
+            success: false,
+            error: "Invalid e-mail",
+            details: {
+                email: email
+            }
+        });
+    }
+    var _koc = this;
+    return this.verify(username, password, password).then( function(result) {
+        if(!result.success || (!result.username.length && !result.password.length)) {
+            // we need a turing
+            return _koc.requestPage("GET", "index.php", null, function(response, body) {
+                // get the turing
+                var turing = getTuring(body);
+                if( turing.length) {
+                    return _koc.requestPage("POST", "signup.php", {
+                        'race'      : race,
+                        'username'  : username,
+                        'password'  : password,
+                        'password2' : password,
+                        'turing'    : turing,
+                        'recaptcha_challenge_field': challenge,
+                        'recaptcha_response_field' : challenge_response
+                    }, function(response, body) {
+                        console.log("great, we almost registered");
+                        //TODO: Send the mail
+                        return _koc.requestPage("POST", "verify.php", {
+                            activation_email: email
+                        }, function(response, body) {
+                            if( response.request.path == '/verify.php?sent=true') {
+                                return {
+                                    success: true,
+                                    message: "Check your e-mails to validate the account",
+                                    KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE
+                                }; 
+                            }
+                            else {
+                                var message = "There was an issue sending the verification mail";
+                                if( response.request.path == "/verify.php?invalid=true" ) {
+                                    message = "Invalid e-mail or already taken";
+                                }
+                                return {
+                                    success: true,
+                                    message: message,
+                                    KOC_SESSION_COOKIE: _koc.KOC_SESSION_COOKIE,
+                                    details: response.request.path
+                                };
+                            }
+                        });
+                    });
+                }
+                else {
+                    return {
+                      success: false,
+                      error: "Could not get a turing",
+                      details: body
+                    };
+                }
+            })
+        }
+        else {
+            // something's wrong with the validation
+            var error = util.format("%s %s", result.username, result.password).trim();
+            return {
+                success: false,
+                error: error,
+                details: result
+            }
+        }
+    });
+};
+
+koc.verify = function(username, password, password2) {
+    return this.requestPage("POST", "signup.php", {
+        username : username,
+        password: password,
+        password2: password2
+    }, function(response, body) {
+        try {
+            var result = JSON.parse(body);
+            result.success = result.username!==undefined&&result.password!==undefined;
+            return result;
+        } catch(e){
+            return {
+                success: false,
+                error: "Error parsing response",
+                details: body
+            };
+        }
+    }, true);
+}
 
 module.exports = koc;
--- test/test.js
+++ test/test.js
@@ -1,29 +1,68 @@
-var should = require('chai').should(),
-    fs     = require('fs'),
-    koc    = require('../koc');
+var chai           = require('chai'),
+    chaiAsPromised = require("chai-as-promised"),
+    fs             = require('fs'),
+    koc            = require('../koc');
+chai.use(chaiAsPromised);
+var should = chai.should();
 
-describe('#battlefield' , function() {
+describe('Parse Battlefield' , function() {
   var htmlPaths = [
+    'test/html/battlefield_full_logged-out.html',
     'test/html/battlefield_xhr_logged-out.html',
-    'test/html/battlefield_full_logged-out.html'
+    'test/html/battlefield_xhr_logged-in.html',
+    'test/html/battlefield_full_first-login.html',
   ];
   for(var iHtml in htmlPaths) {
     var htmlPath = htmlPaths[iHtml];
-    console.log('Parsing battlefield from ' + htmlPath);
-    var html     = fs.readFileSync(htmlPath, 'utf8');
-    var result   = koc.parseBattlefield(html);
-    it('should have 20 records', function() {
-      result.length.should.equal(20);
-    });
-    for(var index in result) {
-      var player = result[index];
-      it('each player should have all properties', function() {
+    describe('#' + htmlPath, function() {
+      var html     = fs.readFileSync(htmlPath, 'utf8');
+      var result   = koc.parseBattlefield(html);
+      it('should return 20 records', function() {
+        return result.length.should.equal(20);
+      });
+      for(var index in result) {
+        var player = result[index];
         var requiredFields = ['userid','alliance','username','armySize','race','gold','rank'];
         for(var i in requiredFields){
-          player.should.have.property(requiredFields[i]);
-          player[requiredFields[i]].should.not.be.undefined;
+          it( "player #" + (index+1) + " should have '" + requiredFields[i] +"'", function() {
+            return player.should.have.property(requiredFields[i])
+              && player[requiredFields[i]].should.not.be.undefined;
+          });
         }
-      });
-    }
+      }
+    } );
   }
 } );
+
+describe('Login' , function() {
+  describe("#wrong credentials", function() {
+    var loginPromise = koc.login("wrong username","wrong password");
+    it('should be fulfilled', function() {
+      return loginPromise.should.be.fulfilled;
+    });
+    it('should have KOC_SESSION_COOKIE field', function() {
+      return loginPromise.should.eventually.have.property("KOC_SESSION_COOKIE");
+    });
+    it('should have success field', function() {
+      return loginPromise.should.eventually.have.property("success");
+    });
+    it('should have error field', function() {
+      return loginPromise.should.eventually.have.property("error");
+    });
+  });
+});
+
+describe('ReCaptcha Image', function() {
+  describe('#get image', function() {
+    var imagePromise = koc.getLoginCaptcha();
+    it('should be fulfilled', function() {
+      return imagePromise.should.be.fulfilled;
+    });
+    it('should have success field', function() {
+      return imagePromise.should.eventually.have.property("success");
+    });
+    it('should have image field', function() {
+      return imagePromise.should.eventually.have.property("image");
+    });
+  })
+});
